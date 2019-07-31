3acc197bb172f83cc33d16b8c601f85862f6b9c1
Emil <emilharoutunian@gmail.com>
2019-07-31 15:47:18 -0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,17 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Emil Haroutunian on 7/31/19.
+//  Copyright © 2019 Swift Dev. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
